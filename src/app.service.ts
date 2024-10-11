c885e0ec137ce49e53a43694ba9dfee8b28706f0
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-10-11 23:28:03 +0300

    refactor: delete some files

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! This is my first backend server by nest.js';
+  }
+  getProfile(): string {
+    return 'This is my profile :)';
   }
 }

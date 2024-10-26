f0736b827d6e0afb441a42618ad9a13a318638c0
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-10-26 11:40:34 +0300

    feat: add configModul global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProfessionModule } from './profession/profession.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProfessionModule, AuthModule],
+  imports: [
+    UserModule,
+    ProfessionModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

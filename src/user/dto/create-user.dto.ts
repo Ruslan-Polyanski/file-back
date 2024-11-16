71685fc3ac3b8e633493f03a2852379b5887682c
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-11-16 20:42:24 +0300

    feat: add user validate structure

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { IsEmail, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @UsePipes(new ValidationPipe())
   email: string;
+
+  @MinLength(6, { message: 'Password must be more then 6 simbols' })
   password: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const existUser = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (existUser) throw BadRequestException('This email already exists');
+
+    const user = await this.userRepository.save({
+      email: createUserDto.email,
+      password: createUserDto.password,
+    });
+
     return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }

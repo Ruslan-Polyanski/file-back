f1cd271aa0f0f028729415efa7f6b82ebb33d466
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2025-02-02 15:26:56 +0300

    clean project

--- src/profession/dto/update-profession.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProfessionDto } from './create-profession.dto';
-
-export class UpdateProfessionDto extends PartialType(CreateProfessionDto) {}
